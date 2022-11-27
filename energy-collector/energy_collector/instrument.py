f82b7f482dc0b0368f1087d1ba81619cbeab8cd8
stesua <>
2022-11-27 14:24:23 +0100

    (WIP) fix import

--- energy-collector/energy_collector/instrument.py
+++ energy-collector/energy_collector/instrument.py
@@ -6,7 +6,7 @@ class OrnaWe515:
     def __init__(self):
         self.instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
         self.instrument.serial.baudrate = 9600
-        self.instrument.serial.parity   = serial.PARITY_EVEN
+        self.instrument.serial.parity   = minimalmodbus.serial.PARITY_EVEN
         self.instrument.serial.timeout  = 0.10          # seconds
         self.instrument.debug = False
 
