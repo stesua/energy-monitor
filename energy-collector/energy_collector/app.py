bbf83309816ced9cab3f5ae3af61409d099760a9
stesua <>
2022-11-28 21:21:32 +0100

    (WIP) draft influxdb writer

--- energy-collector/energy_collector/app.py
+++ energy-collector/energy_collector/app.py
@@ -1,21 +1,23 @@
 import logging
-from influxdb import InfluxDBClient
-from collect import SmartMeterCollector
+from influxdb_client import InfluxDBClient
+from collect import SmartMeterCollector, Measure, MeasureException
 from instrument import OrnaWe515
 
 if __name__ == '__main__':
-
-    # client = InfluxDBClient(influx_config['host'],
-    #                         influx_config['port'],
-    #                         influx_config['user'],
-    #                         influx_config['password'],
-    #                         influx_config['dbname'])
-
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("Start monitoring")
+    # TODO: change
+    token = "iGHIoYd-jIJ1piThJevsDcPmpSQzo3pV45GL0sSE0c34ESsP30ChBXSYQ0HP6IZhK6zgqI-cbuYLGWfWZ4iZNA=="
+    org = "home"
+    bucket = "energy"
+    url = "http://localhost:8086"
+    influxdb_client = InfluxDBClient(url=url, token=token, org=org)
+    
     collector = SmartMeterCollector(smart_meter = OrnaWe515())
 
     try:
         measure = collector.collect()
-        logging.debug(measure)
+        logging.info(measure)
     except MeasureException as e:
         logging.error(e)
 
