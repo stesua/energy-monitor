06d2ccb1e1a7e8d895c473b79d185e7992286637
stesua <>
2022-12-01 00:00:14 +0100

    misc improvement, refactoring and cleanup

--- energy-collector/energy_collector/app.py
+++ energy-collector/energy_collector/app.py
@@ -16,9 +16,9 @@ def run_job():
     logging.debug(f"monitoring job triggered at {datetime.utcnow()}")
     # TODO: change
     # raspberry
-    token = "zBrdmVUqqKmgSkjmJdVavMsdsJrAZv7znzR-s6sYPNidNwjsno8g-eQhropAJlbj2gq_1zyUpZxxN1vZH8YPgA=="
+    # token = "zBrdmVUqqKmgSkjmJdVavMsdsJrAZv7znzR-s6sYPNidNwjsno8g-eQhropAJlbj2gq_1zyUpZxxN1vZH8YPgA=="
     # mac
-    # token = "OZxcsRhjMmh0TpJZxrcya_wZJFJPwQ_QExVbreH2XqC7wans6G_Fs87-Uu7UhvG-TwsxJHa4g9U-4hRBH3h3TQ=="
+    token = "influx-db-token"
 
     org = "home"
     bucket = "energy"
@@ -26,12 +26,12 @@ def run_job():
     influxdb_service = InfluxDBService(org, bucket, url, token)
 
     # TODO: use factory instead
-    collector = OrnaWe515Collector()
-    # collector = FakeCollector()
+    # collector = OrnaWe515Collector()
+    collector = FakeCollector()
 
     try:
         measure = collector.collect()
-        logging.info(measure)
+        logging.debug(measure)
         influxdb_service.write_measure(measure)
     except Exception:
         logging.exception("collecting failure")
--- energy-collector/energy_collector/instrument.py
+++ energy-collector/energy_collector/instrument.py
@@ -6,8 +6,10 @@ class OrnaWe515:
     def __init__(self):
         self.instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
         self.instrument.serial.baudrate = 9600
-        self.instrument.serial.parity   = minimalmodbus.serial.PARITY_EVEN
-        self.instrument.serial.timeout  = 0.10          # seconds
+        self.instrument.serial.parity = minimalmodbus.serial.PARITY_EVEN
+
+        # TODO try to increase timeout to avoid: minimalmodbus.NoResponseError: No communication with the instrument (no answer)
+        self.instrument.serial.timeout = 0.10          # seconds
         self.instrument.debug = False
 
     
